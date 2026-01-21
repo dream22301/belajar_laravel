4ec2167d7f5adad9286ab16e142e1b8fb184a1b2
dream22301 <brilliantprincoekananda2009@gmail.com>
2026-01-21 10:09:03 +0700

    Commit ke 9

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -9,6 +9,12 @@ class FormController extends Controller
     public function kasih(Request $request) {
         $nama = $request->input('nama');
 
-        return $nama;
+        $request->validate([
+            'nama' => 'required|min:2|max:20'
+        ]);
+
+        return view('form-submit', [
+            'nama' => $request->nama
+        ]);
     }
 }
---
+++ resources/views/form-submit.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title','form-submit')
+
+@section('header')
+    <h1>Ini Form</h1>
+@endsection
+
+@section('content')
+    <p>{{ $nama }}</p>
+@endsection
